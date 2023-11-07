490002356ad716c53a79bfa5400948c4709ac24b
habibur rahman <habiburrahmanofficial157@gmail.com>
2023-11-08 00:17:21 +0600

    added to extra section home page and show rating and confarm job , new user and new register

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,10 +4,18 @@ import HomePageTabs from "./HomePageTabs";
 import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import Loading from "../../ShearPages/Loading/Loading";
+import { useEffect } from "react";
 
 const Home = () => {
   const [data, setData] = useState([]);
+  const [about, setAbout] = useState([]);
 
+  useEffect(() => {
+    fetch("about.json")
+      .then((res) => res.json())
+      .then((data) => setAbout(data));
+  }, []);
+  console.log(about);
   const { isLoading } = useQuery({
     queryKey: ["repoData"],
     queryFn: () =>
@@ -17,7 +25,7 @@ const Home = () => {
   });
 
   if (isLoading) return <Loading></Loading>;
-  console.log(data);
+
   return (
     <div>
       <Helmet>
@@ -51,6 +59,95 @@ const Home = () => {
         </div>
       </div>
 
+      <div className="pb-5">
+        <h1 className="text-xl md:text-4xl text-center font-bold text-blue-500 py-5">
+          What Our Customers are Saying About Us
+        </h1>
+
+        <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 text-center items-center py-2 md:py-5 gap-5 md:gap-10 ">
+          {about?.map((item) => (
+            <div
+              key={item.id}
+              className="card h-56 bg-base-100 shadow-xl border border-blue-500">
+              <figure className="px-10 py-5 pt-10">
+                <div className="avatar mr-2">
+                  <div className="w-12 rounded-full">
+                    <img src={item.image} />
+                  </div>
+                </div>
+                <h2 className="card-title text-orange-500 font-semibold">
+                  {item.name}
+                  <span className="text-black font-extralight text-sm">
+                    {item.company}
+                  </span>
+                </h2>
+              </figure>
+              <p className="px-2">{item.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="flex justify-center items-center  py-10">
+        <div className="stats stats-vertical lg:stats-horizontal shadow ">
+          <div className="stat">
+            <div className="stat-figure text-orange-500">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block w-8 h-8 stroke-current">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+              </svg>
+            </div>
+            <div className="stat-title">Confirm Jobs</div>
+            <div className="stat-value">31K</div>
+            <div className="stat-desc">Jan 1st - Aug 1st</div>
+          </div>
+
+          <div className="stat">
+            <div className="stat-figure text-orange-500">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block w-8 h-8 stroke-current">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path>
+              </svg>
+            </div>
+            <div className="stat-title">New Users</div>
+            <div className="stat-value">4,200</div>
+            <div className="stat-desc">↗︎ 400 (22%)</div>
+          </div>
+
+          <div className="stat">
+            <div className="stat-figure text-orange-500">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block w-8 h-8 stroke-current">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path>
+              </svg>
+            </div>
+            <div className="stat-title">New Registers</div>
+            <div className="stat-value">1,200</div>
+            <div className="stat-desc">↘︎ 90 (14%)</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
